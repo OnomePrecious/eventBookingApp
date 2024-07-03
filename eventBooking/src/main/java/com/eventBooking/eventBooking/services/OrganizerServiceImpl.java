2aab7f0afc17261f8e73c7ecf9ed647ce0f879d7
Precious <preciousonome002@gmail.com>
2024-07-03 12:01:02 +0100

    "smallEventBookingApp"

---
+++ eventBooking/src/main/java/com/eventBooking/eventBooking/services/OrganizerServiceImpl.java
@@ -0,0 +1,26 @@
+package com.eventBooking.eventBooking.services;
+
+import com.eventBooking.eventBooking.dtos.Request.AddTicketToEventRequest;
+import com.eventBooking.eventBooking.dtos.Request.CreateAnEventRequest;
+import com.eventBooking.eventBooking.dtos.Request.RegisterRequest;
+import com.eventBooking.eventBooking.dtos.Response.AddTicketToEventResponse;
+import com.eventBooking.eventBooking.dtos.Response.CreateAnEventResponse;
+import com.eventBooking.eventBooking.dtos.Response.RegisterResponse;
+
+public class OrganizerServiceImpl implements OrganizerService {
+    @Override
+    public RegisterResponse registerOrganizer(RegisterRequest registerRequest) {
+        return null;
+    }
+
+    @Override
+    public CreateAnEventResponse createEvent(CreateAnEventRequest createEventRequest) {
+        return null;
+    }
+
+    @Override
+    public AddTicketToEventResponse addTicketToEvent(AddTicketToEventRequest addTicketToEventRequest) {
+        return null;
+    }
+
+}

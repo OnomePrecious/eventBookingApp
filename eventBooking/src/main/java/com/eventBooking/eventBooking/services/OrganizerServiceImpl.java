8a415432f43007e0b4d2a71542cfc4d88aaed0e6
Precious <preciousonome002@gmail.com>
2024-07-03 12:26:14 +0100

    "smallEventBookingApp"

--- eventBooking/src/main/java/com/eventBooking/eventBooking/services/OrganizerServiceImpl.java
+++ eventBooking/src/main/java/com/eventBooking/eventBooking/services/OrganizerServiceImpl.java
@@ -9,19 +9,22 @@ import com.eventBooking.eventBooking.dtos.Response.CreateAnEventResponse;
 import com.eventBooking.eventBooking.dtos.Response.RegisterResponse;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 @Service
 public class OrganizerServiceImpl implements OrganizerService {
     private final ModelMapper modelMapper;
+    private final PasswordEncoder passwordEncoder;
     @Autowired
-    public OrganizerServiceImpl(ModelMapper modelMapper){
+    public OrganizerServiceImpl(ModelMapper modelMapper, PasswordEncoder passwordEncoder){
         this.modelMapper = modelMapper;
+        this.passwordEncoder = passwordEncoder;
     }
     @Override
     public RegisterResponse registerOrganizer(RegisterRequest registerRequest) {
         Organizer organizer = modelMapper.map(registerRequest, Organizer.class);
         organizer.setUsername(registerRequest.getUsername());
-        organizer.setPassword(registerRequest.getPassword());
+        organizer.setPassword(passwordEncoder.encode(registerRequest.getPassword()));
         organizer.setEmail(registerRequest.getEmail());
         RegisterResponse registerResponse = modelMapper.map(registerRequest, RegisterResponse.class);
         registerResponse.setMessage("Registration successful");
--- eventBooking/src/test/java/com/eventBooking/eventBooking/services/OrganizerServiceImplTest.java
+++ eventBooking/src/test/java/com/eventBooking/eventBooking/services/OrganizerServiceImplTest.java
@@ -1,14 +1,26 @@
 package com.eventBooking.eventBooking.services;
 
+import com.eventBooking.eventBooking.dtos.Request.RegisterRequest;
+import com.eventBooking.eventBooking.dtos.Response.RegisterResponse;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class OrganizerServiceImplTest {
+    @Autowired
+    private OrganizerService organizerService;
 
     @Test
-    void registerOrganizer() {
+    void testThatAnOrganizerCanRegister() {
+        RegisterRequest registerRequest = new RegisterRequest();
+        registerRequest.setUsername("my username");
+        registerRequest.setEmail("myname@gmail.com");
+        registerRequest.setPassword("my password");
+        RegisterResponse registerResponse = organizerService.registerOrganizer(registerRequest);
+        assertNotNull(registerResponse);
+        assertTrue(registerResponse.getMessage().contains("success"));
     }
 
     @Test

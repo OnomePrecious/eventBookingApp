f95c25ee66551addb57e5755655285ac8592bcc1
Precious <preciousonome002@gmail.com>
2024-07-03 11:44:15 +0100

    "smallEventBookingApp"

--- eventBooking/src/main/java/com/eventBooking/eventBooking/data/models/Event.java
+++ eventBooking/src/main/java/com/eventBooking/eventBooking/data/models/Event.java
@@ -1,4 +1,12 @@
 package com.eventBooking.eventBooking.data.models;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
 public class Event {
+    private Long id;
+    private EventType typeOfEvent;
 }

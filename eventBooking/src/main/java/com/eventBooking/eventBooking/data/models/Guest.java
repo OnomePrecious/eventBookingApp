85b3aeb1a0bfd5bea90c2916850c3d7216e3bbd9
Precious <preciousonome002@gmail.com>
2024-07-03 12:33:12 +0100

    "smallEventBookingApp"

---
+++ eventBooking/src/main/java/com/eventBooking/eventBooking/data/models/Guest.java
@@ -0,0 +1,11 @@
+package com.eventBooking.eventBooking.data.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Guest {
+    private Long id;
+    private String name;
+}
--- eventBooking/src/main/java/com/eventBooking/eventBooking/data/models/Organizer.java
+++ eventBooking/src/main/java/com/eventBooking/eventBooking/data/models/Organizer.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
@@ -25,6 +26,8 @@ import static jakarta.persistence.GenerationType.IDENTITY;
         @ElementCollection
         @Enumerated(EnumType.STRING)
         private List<EventType> event;
+        @OneToMany
+        private List<Guest> guestList = new ArrayList<>()
 
 
 

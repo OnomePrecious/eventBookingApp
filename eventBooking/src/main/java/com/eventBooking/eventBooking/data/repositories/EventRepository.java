d5cfc0a8d2508680afd43a0dacb5bf5c876d5eaa
Precious <preciousonome002@gmail.com>
2024-07-04 08:00:29 +0100

    "smallEventBookingApp"

--- eventBooking/src/main/java/com/eventBooking/eventBooking/data/repositories/EventRepository.java
+++ eventBooking/src/main/java/com/eventBooking/eventBooking/data/repositories/EventRepository.java
@@ -4,4 +4,5 @@ import com.eventBooking.eventBooking.data.models.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EventRepository extends JpaRepository<Event, Long> {
+    Event findGuestsBy(Long eventId);
 }
